4497f8c835201f500102c95806341a92fca336fa
Charchit Varshney <charchit.varshney@accenture.com>
2020-05-06 14:03:09 +0000

    commit changes with Proper Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,248 +1,222 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener, ChangeDetectorRef, NgZone } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'SnackGame';
+export class AppComponent implements OnInit {
+    title = 'Snake And Apple';
 
-  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>; 
-  @HostListener('window:keyup', ['$event']) keyEvent(eve: KeyboardEvent) {
-    this.keyPrerssed(eve);
-    console.log(eve);
-  }
-  private ctx: CanvasRenderingContext2D;
-  imageName;
-   snake: any[]=[];
-   box: number =20;
-  applePosition: any[]=[];
-  score: number =0;
-  direction :string;
-  currentPositionX: number;
-  currentPositionY: number;
-  foodPositionX: number;
-  foodPositionY:number;
-  horizontalMovement: boolean =false;
-  verticalMovement:boolean =false;
-  newWidth: number =0;
-  newHeight: number =0;
-  i;
-  constructor(private cdk: ChangeDetectorRef, private zone : NgZone){
-
-  }
-
-
-  ngOnInit(): void {
-    this.imageName = new Image();
-    this.imageName.src="../assets/apple.jpg"
-    this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.ctx.fillStyle = 'red';
-    this.ctx.drawImage(this.imageName,260,140,20,20);
-    this.animate();
-    this.foodPositionX=(Math.floor(Math.random() * 30));
-    this.foodPositionY=(Math.floor(Math.random() * 30));
-  }
-  animate() {
-    this.ctx.fillStyle = 'red';
-
-    //this.move(1,20);
-   this.moveNew(1,8)
-
-  }
-
-
-  keyPrerssed(eve){
-     if(eve.keyCode === 37 && this.direction !='right'){
-      this.direction = 'left';
-     }
-     else if(eve.keyCode === 38 && this.direction !='down'){
-      this.direction = 'up';
-     }
-     else if(eve.keyCode === 39 && this.direction !='left'){
-      this.direction = 'right';
-     }
-     else if(eve.keyCode === 40 && this.direction !='up'){
-      this.direction = 'down';
-     }
-     if(this.direction === 'left'){
-     this.currentPositionX-=1;
-     clearInterval(this.i);
-      this.verticalMovement = false;
-      this.horizontalMovement = true;
-     this.moveNewForXLeft(this.currentPositionX, this.currentPositionY);
-     
-       
-     }
-     if(this.direction === 'up'){
-      //snakeY -=this.box;
-      this.currentPositionY-=1;
-      clearInterval(this.i);
-      this.verticalMovement = true;
-      this.horizontalMovement = false;
-      this.moveNewForYUpwards(this.currentPositionX, this.currentPositionY);
-      
-    }
-
-    if(this.direction === 'right'){
-      //snakeX +=this.box;
-      this.currentPositionX+=1;
-      clearInterval(this.i);
-      this.verticalMovement = false;
-      this.horizontalMovement = true;
-      this.moveNew(this.currentPositionX, this.currentPositionY);
-      
-    }
-
-    if(this.direction === 'down'){
-     this.currentPositionY+=1;
-     clearInterval(this.i);
-     this.verticalMovement = true;
-     this.horizontalMovement = false;
-     this.moveNewForYDownward(this.currentPositionX, this.currentPositionY);
-    }
-    
-    //this.moveNew(this.currentPositionX, this.currentPositionY);
-    let i= this.snake.length;
-
+    @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+    @HostListener('window:keyup', ['$event']) keyEvent(eve: KeyboardEvent) {
+        this.keyPrerssed(eve);
 
     }
+    private ctx: CanvasRenderingContext2D;
+    imageName;
+    box: number = 20;
+    applePosition: any[] = [];
+    score: number = 1;
+    scoreTodisplay: number = 0;
+    direction: string;
+    currentPositionX: number;
+    currentPositionY: number;
+    foodPositionX: number;
+    foodPositionY: number;
+    interval;
+    tail: any[] = [];
 
-  draw(x: number, y: number, z: number, newWidth, newHeight) {
-    this.currentPositionX=x;
-    this.currentPositionY=y;
-    if(x === this.foodPositionX && y=== this.foodPositionY){
-      this.foodPositionX=(Math.floor(Math.random() * 30));
-      this.foodPositionY=(Math.floor(Math.random() * 30));
-      this.score++;
-      // if(this.verticalMovement){
-      //  this.newHeight=z*2;
-      //   this.ctx.fillRect(z * x, z * y, z, z*2);
-      // }
-      // else if(this.horizontalMovement){
-      // this.newWidth=z*2;
-      //   this.ctx.fillRect(z * x, z * y, z*2, z);
-      // }
+    constructor() {
     }
-    // if(newHeight !=0){
-    //   this.ctx.fillRect(z * x, z * y, z, newHeight);
-    // }
-    // else if(newWidth!=0){
-    //   this.ctx.fillRect(z * x, z * y, newWidth, z);
-    // }
-    // else{
-      this.ctx.fillRect(z * x, z * y, z, z);
-    
-   
-    
 
-  }
-    
-  // move(y: number, z: number) {
-  //   const max = this.ctx.canvas.width / z;
-  //   const canvas = this.ctx.canvas;
-  //   let x = 0;
-  //   const i = setInterval(() => {
-  //     this.ctx.clearRect(0, 0, canvas.width, canvas.height);   
-  //     this.ctx.drawImage(this.imageName,260,140,20,20);   
-  //     this.draw(x, y, z);
-
-  //     x++;
-  //     if (x >= max) {
-  //       clearInterval(i);
-  //       this.move(y,z);
-  //     }
-  //   }, 200);    
-  // }
-
-  moveNew(x: number, y: number) {
-   // const max = this.ctx.canvas.width / z;
-    const max= 30
-    const canvas = this.ctx.canvas;
-
-;
-  
-   this.i = setInterval(() => {
-    this.ctx.clearRect(0, 0, canvas.width, canvas.height);   
-    this.ctx.drawImage(this.imageName,this.foodPositionX *20,this.foodPositionY*20,20,20);  
-    
-    this.draw(x, y, 20, this.newWidth, this.newHeight);
-    x++; 
- 
-    if (x >= max) {
-      clearInterval(this.i);
-      this.moveNew(0,y);
+    ngOnInit(): void {
+        this.imageName = new Image();
+        this.imageName.src = "../assets/apple.jpg"
+        this.ctx = this.canvas.nativeElement.getContext('2d');
+        this.ctx.fillStyle = 'red';
+        this.animate();
+        this.foodPositionX = (Math.floor(Math.random() * 30));
+        this.foodPositionY = (Math.floor(Math.random() * 30));
     }
-  }, 200);   
- 
-   
+
+    animate() {
+        this.ctx.fillStyle = 'red';
+        this.moveRightInHorizonatlDirection(1, 8)
+    }
+
+    keyPrerssed(eve) {
+        if (eve.keyCode === 37 && this.direction != 'right') {
+            this.direction = 'left';
+        }
+        else if (eve.keyCode === 38 && this.direction != 'down') {
+            this.direction = 'up';
+        }
+        else if (eve.keyCode === 39 && this.direction != 'left') {
+            this.direction = 'right';
+        }
+        else if (eve.keyCode === 40 && this.direction != 'up') {
+            this.direction = 'down';
+        }
+        if (this.direction === 'left') {
+            this.currentPositionX -= 1;
+            clearInterval(this.interval);
+            this.moveLeftInHorizontalDirection(this.currentPositionX, this.currentPositionY);
+        }
+        if (this.direction === 'up') {
+            this.currentPositionY -= 1;
+            clearInterval(this.interval);
+
+            this.moveUpInVerticalDirection(this.currentPositionX, this.currentPositionY);
+
+        }
+
+        if (this.direction === 'right') {
+            this.currentPositionX += 1;
+            clearInterval(this.interval);
+            this.moveRightInHorizonatlDirection(this.currentPositionX, this.currentPositionY);
+        }
+        if (this.direction === 'down') {
+            this.currentPositionY += 1;
+            clearInterval(this.interval);
+            this.moveDownInVerticalDirection(this.currentPositionX, this.currentPositionY);
+        }
+    }
+
+    draw(x: number, y: number, z: number) {
+        let tail1: any[] = [];
+        this.currentPositionX = x;
+        this.currentPositionY = y;
+        const head = this.tail[0];
+        if (this.iSCollisionOccur()) {
+            alert('Game over');
+            clearInterval(this.interval);
+            this.animate();
+            this.scoreTodisplay=0;
+            this.score=0;
+            return;
+        }
+        else {
+            if (x === this.foodPositionX && y === this.foodPositionY) {
+                this.foodPositionX = (Math.floor(Math.random() * 30));
+                this.foodPositionY = (Math.floor(Math.random() * 30));
+                this.score++;
+                this.scoreTodisplay++;
+            }
+
+            this.ctx.fillStyle = 'green';
+            this.ctx.fillRect(z * x, z * y, z, z);
+            for (let i = 0; i < this.tail.length - 1; i++) {
+
+                if (this.score > 0) {
+                    this.ctx.strokeStyle = 'black';
+                    if (this.tail[i]) {
+                        this.ctx.strokeRect(z * this.tail[i].x, z * this.tail[i].y, z, z);
+                    }
+                }
+            }
+        }
+    }
+
+    moveRightInHorizonatlDirection(x: number, y: number) {
+        const max = 30
+        const canvas = this.ctx.canvas;
+        this.interval = setInterval(() => {
+            this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.ctx.drawImage(this.imageName, this.foodPositionX * 20, this.foodPositionY * 20, 20, 20);
+            for (let i = 0; i < this.tail.length; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+            this.tail[this.score - 1] = { x: x, y: y };
+            this.draw(x, y, 20);
+            x++;
+            if (x >= max) {
+                clearInterval(this.interval);
+                this.moveRightInHorizonatlDirection(0, y);
+            }
+        }, 100);
+    }
+
+    moveDownInVerticalDirection(x: number, y: number) {
+        const max = 30
+        const canvas = this.ctx.canvas;
+        this.interval = setInterval(() => {
+            this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.ctx.drawImage(this.imageName, this.foodPositionX * 20, this.foodPositionY * 20, 20, 20);
+            for (let i = 0; i < this.tail.length; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+            this.tail[this.score - 1] = { x: x, y: y };
+            this.draw(x, y, 20);
+            y++;
+
+            if (y >= max) {
+                clearInterval(this.interval);
+                this.moveDownInVerticalDirection(x, 0);
+            }
+        }, 100);
+
+
+    }
+
+    moveUpInVerticalDirection(x: number, y: number) {
+        const max = 20;
+        const min = -1;
+        const canvas = this.ctx.canvas;
+
+
+        this.interval = setInterval(() => {
+            this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.ctx.drawImage(this.imageName, this.foodPositionX * 20, this.foodPositionY * 20, 20, 20);
+            for (let i = 0; i < this.tail.length; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+            this.tail[this.score - 1] = { x: x, y: y };
+            this.draw(x, y, 20);
+            y--;
+            if (y <= min) {
+                clearInterval(this.interval);
+                this.moveUpInVerticalDirection(x, 29);
+            }
+        }, 100);
+
+
+    }
+    moveLeftInHorizontalDirection(x: number, y: number) {
+        const max = 20;
+        const min = -1;
+        const canvas = this.ctx.canvas;
+
+
+        this.interval = setInterval(() => {
+            this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+            this.ctx.drawImage(this.imageName, this.foodPositionX * 20, this.foodPositionY * 20, 20, 20);
+            for (let i = 0; i < this.tail.length; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+
+            this.tail[this.score - 1] = { x: x, y: y };
+            this.draw(x, y, 20);
+            x--;
+            if (x <= min) {
+                clearInterval(this.interval);
+                this.moveLeftInHorizontalDirection(29, y);
+            }
+        }, 100);
+    }
+
+    iSCollisionOccur() {
+        for (let i = 4; i < this.tail.length; i++) {
+            if (this.tail[i] && this.tail[0]) {
+                if (this.tail[i].x === this.tail[0].x && this.tail[i].y === this.tail[0].y) {
+                    return true;
+                }
+            }
+
+        }
+    }
 }
 
-moveNewForYDownward(x: number, y: number) {
-  // const max = this.ctx.canvas.width / z;
-   const max= 30
-   const canvas = this.ctx.canvas;
 
- 
-  this.i = setInterval(() => {
-   this.ctx.clearRect(0, 0, canvas.width, canvas.height);   
 
-   this.ctx.drawImage(this.imageName,this.foodPositionX *20,this.foodPositionY*20,20,20); 
-   this.draw(x, y, 20, this.newWidth, this.newHeight);
-   y++; 
 
-   if (y >= max) {
-     clearInterval(this.i);
-     this.moveNewForYDownward(x,0);
-   }
- }, 200); 
-
-  
-}
-
-moveNewForYUpwards(x: number, y: number) {
-  // const max = this.ctx.canvas.width / z;
-   const max= 20;
-   const min=-1;
-   const canvas = this.ctx.canvas;
-
- 
-  this.i = setInterval(() => {
-   this.ctx.clearRect(0, 0, canvas.width, canvas.height);   
-   this.ctx.drawImage(this.imageName,this.foodPositionX *20,this.foodPositionY*20,20,20);  
-   this.draw(x, y, 20, this.newWidth, this.newHeight);
-   y--; 
-
-   if (y <= min) {
-     clearInterval(this.i);
-     this.moveNewForYUpwards(x,30);
-   }
- }, 200); 
-
-  
-}
-moveNewForXLeft(x: number, y: number) {
-  // const max = this.ctx.canvas.width / z;
-   const max= 20;
-   const min =-1;
-   const canvas = this.ctx.canvas;
-
- 
-  this.i = setInterval(() => {
-   this.ctx.clearRect(0, 0, canvas.width, canvas.height);   
-   this.ctx.drawImage(this.imageName,this.foodPositionX *20,this.foodPositionY*20,20,20); 
-   this.draw(x, y, 20, this.newWidth, this.newHeight);
-   x--; 
-
-   if (x <= min) {
-     clearInterval(this.i);
-     this.moveNewForXLeft(30,y);
-   }
- }, 200); 
-
-  
-}
-     
-  
-}
